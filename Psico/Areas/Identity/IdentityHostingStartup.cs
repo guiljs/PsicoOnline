7b175900630b3fec1eeeb2d78f97cbce3aef2505
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-05-21 17:10:10 -0300

    Scaffold do Identity

---
+++ Psico/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Psico.Data;
+
+[assembly: HostingStartup(typeof(Psico.Areas.Identity.IdentityHostingStartup))]
+namespace Psico.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
